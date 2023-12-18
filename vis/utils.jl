ba3a1addfa32bcf31cd437b438564d60e0fbc9ef
math4mad <smartfamilycn@qq.com>
2023-12-19 00:20:27 +0800

    add note
    add note

--- vis/utils.jl
+++ vis/utils.jl
@@ -2,5 +2,6 @@ using CairoMakie,CSV,DataFrames,Pipe,Tidier,HypertextLiteral
 using MakieThemes
 using StatsBase
 using AlgebraOfGraphics
+using RCall
 Makie.set_theme!(ggthemr(:dust))
 @info "loading success"
