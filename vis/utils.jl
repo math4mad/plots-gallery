4ab8b624ee7e6dcb1bc6ed3378fa0cfbbf0e4805
math4mad <smartfamilycn@qq.com>
2023-12-19 16:32:26 +0800

    add note
    add note

--- vis/utils.jl
+++ vis/utils.jl
@@ -4,4 +4,13 @@ using StatsBase
 using AlgebraOfGraphics
 using RCall
 Makie.set_theme!(ggthemr(:dust))
+
+
+function make_title(str::String)
+    return @htl("""
+    <div class="alert alert-primary" role="alert">
+       $(str)
+    </div>
+""")
+end
 @info "loading success"
